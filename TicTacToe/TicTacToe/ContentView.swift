9f3f602b597e42d43bccbc95075f510e78d96e47
Luka Sarcevic <52749091+sarcevic-luka@users.noreply.github.com>
2023-10-04 00:40:48 +0200

    Add files via upload

--- TicTacToe/TicTacToe/ContentView.swift
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -10,14 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                TicOrToeView()
+                TicOrToeView()
+                TicOrToeView(ticOrToe: .tac)
+            }
+            .background(.black)
+            HStack {
+                Text("Player 1")
+                Spacer()
+                Text("Player 2")
+            }
+            .font(.largeTitle)
         }
         .padding()
     }
 }
+
+struct TicOrToeView: View {
+    var ticOrToe: TicOrToe = .empty
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .opacity(1)
+            Image(systemName: ticOrToe.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(10)
+        }
+    }
+    
+    enum TicOrToe: String {
+        case tac = "circle"
+        case toe = "xmark"
+        case empty = ""
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
