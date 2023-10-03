0523924d2e8aa8d6ee039e3c01789ca57457ad4f
Luka Sarcevic <52749091+sarcevic-luka@users.noreply.github.com>
2023-10-03 23:43:00 +0200

    Add files via upload
    - initial commit to create project

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Code Forge on 03.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
