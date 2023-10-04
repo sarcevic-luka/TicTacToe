9a4bf05fc9ff42fd698a721016d0ef4c4206318a
Luka Sarcevic <lukasarcevic.business@gmail.com>
2023-10-04 11:30:33 +0200

    MV preparation
    - improved UI logic a little bit by separating subviews

--- TicTacToe/TicTacToe/ContentView.swift
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -10,12 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                TicOrToeView()
-                TicOrToeView()
-                TicOrToeView(ticOrToe: .tac)
-            }
-            .background(.black)
+            boardView
             HStack {
                 Text("Player 1")
                 Spacer()
@@ -23,32 +18,51 @@ struct ContentView: View {
             }
             .font(.largeTitle)
         }
-        .padding()
+        .padding(10)
+    }
+    
+    var boardView: some View {
+        GeometryReader { geometry in
+            let size = min(geometry.size.width, geometry.size.height) / 3 - 2*15
+            VStack(spacing: 15) {
+                ForEach(0..<3) { row in
+                    HStack(spacing: 15) {
+                        ForEach(0..<3) { stack in
+                            TicOrToeView()
+                        }
+                    }
+                }
+            }
+            .background(.black)
+        }
+        .aspectRatio(1/1, contentMode: .fit)
     }
 }
 
 struct TicOrToeView: View {
-    var ticOrToe: TicOrToe = .empty
+    @State var ticOrToe: TicOrToe = .empty
     
     var body: some View {
         ZStack {
             Rectangle()
-                .foregroundColor(.white)
+                .fill(.white)
                 .opacity(1)
             Image(systemName: ticOrToe.rawValue)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .padding(10)
         }
+        .onTapGesture {
+            ticOrToe = ticOrToe == .cross ? .nought : .cross
+        }
     }
     
     enum TicOrToe: String {
-        case tac = "circle"
-        case toe = "xmark"
+        case cross = "xmark"
+        case nought = "circle"
         case empty = ""
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
